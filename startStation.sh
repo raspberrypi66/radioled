bb183c429e1d7c1b782bfabc24b7967d1b5406b3
raspberrypi66 <raspberrypi66@gmail.com>
2016-08-08 18:43:24 +0700

    ss

--- startStation.sh
+++ startStation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+kill $(ps -A -o cmd,pid | egrep "setWifi" | head -n 1 | sed -r -e 's/.* ([0-9]+)$/\1/')
 ifdown wlan0
 cat /etc/network/interfaces.station > /etc/network/interfaces
 /etc/init.d/hostapd stop 
